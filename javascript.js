d4323b542ac9d31537b1183830c37bde65f62d86
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2025-05-02 14:17:01 -0400

    Add javascript to retrive information from API
    Created a simple async function to fetch information about the weather
    in toronto and log it onto console.

---
+++ javascript.js
@@ -0,0 +1,34 @@
+//find location and return weather data
+async function getWeather() {
+  
+    try {   
+        const apiKey = '9WDMMPUPV5SG59HXRLFBW949G';
+        const location = myLocation(); 
+        const url = (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${apiKey}&unitGroup=metric`);
+        const response = await fetch (url);
+        if (!response.ok)
+            throw new Error(`http error!: ${response.status}`);
+        const weatherData = await response.json();
+        return weatherData; 
+        } catch (error){
+            console.error("error fetching data:",error);       
+         }
+    }       
+
+
+function myLocation (){
+     const where = "Toronto";
+     return where; 
+}
+
+async function renderWeather() {
+    const weatherData = await getWeather();
+    console.log(weatherData);
+    console.log(weatherData.currentConditions.temp)
+    console.log(weatherData.currentConditions.conditions);
+        
+    }
+
+renderWeather();
+
+
